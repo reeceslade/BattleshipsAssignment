a96f92344d1a95228071ffe1bb8f03a44f70a41b
eyauh <reecesladey@outlook.com>
2023-04-05 13:26:34 +0100

    drawn ships needs some adjustments

--- logic/src/main/kotlin/Bships/StudentShip.kt
+++ logic/src/main/kotlin/Bships/StudentShip.kt
@@ -3,23 +3,12 @@ package Bships
 import uk.ac.bournemouth.ap.battleshiplib.Ship
 import uk.ac.bournemouth.ap.lib.matrix.ext.Coordinate
 
-enum class StudentShip(
+ class StudentShip(
     override val top: Int,
     override val left: Int,
     override val bottom: Int,
     override val right: Int
 ) : Ship {
-    DESTROYER(0, 0, 1, 0),
-    //2 holes
-    SUBMARINE(0, 1, 2, 1),
-    //3 holes
-    CRUISER(2, 2, 4, 2),
-    //3 holes
-    BATTLESHIP(3, 5, 7, 5),
-    //4 holes
-    CARRIER(6, 8, 12, 8);
-    //5 holes
-
     override val columnIndices: IntRange
         get() = super.columnIndices
     override val rowIndices: IntRange
