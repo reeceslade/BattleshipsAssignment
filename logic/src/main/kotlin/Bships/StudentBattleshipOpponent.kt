47c9d4a609b28e9180f428d57d7dcb2d98647424
reeceslade <reecesladey@outlook.com>
2023-04-12 10:05:21 +0100

    directions in opponent

--- logic/src/main/kotlin/Bships/StudentBattleshipOpponent.kt
+++ logic/src/main/kotlin/Bships/StudentBattleshipOpponent.kt
@@ -13,39 +13,49 @@ class StudentBattleshipOpponent(
 ) : BattleshipOpponent {
     init {
         require(ships.all { it.top in 0 until rows && it.bottom in 0 until rows && it.left in 0 until columns && it.right in 0 until columns }) { "Ships must be within the bounds of the game board" }
-        val carrier = StudentShip(0, 0, 0, 4) // creates a ship at coordinates (0,0) to (0,4)
-        val battleship = StudentShip(1, 1, 4, 1) // creates a ship at coordinates (1,1) to (4,1)
-        val cruiser = StudentShip(2, 2, 4, 2) // creates a ship at coordinates (2,2) to (4,2)
-        val submarine = StudentShip(3, 3, 5, 3) // creates a ship at coordinates (3,3) to (5,3)
-        val destroyer = StudentShip(4, 4, 5, 4) // creates a ship at coordinates (4,4) to (5,4)
-        val ships = listOf(carrier, battleship, cruiser, submarine, destroyer)
-
     }
     fun createOpponent( columns: Int,
                         rows: Int,
                         shipSizes: IntArray,
                         random: Random
     ): StudentBattleshipOpponent {
-
         val maxShipSize = shipSizes.maxOrNull() ?: 0
         require(rows >= maxShipSize) { "The number of rows must be greater than or equal to the maximum ship size" }
         require(columns >= maxShipSize) { "The number of columns must be greater than or equal to the maximum ship size" }
-
         val ships = mutableListOf<StudentShip>()
         for (size in shipSizes) {
             var ship: StudentShip
+            val direction: Boolean = if (size > columns) {
+                false // Vertical placement
+            } else if (size > rows) {
+                true // Horizontal placement
+            } else {
+                random.nextBoolean()
+            }
             do {
-                val top = random.nextInt(rows - size + 1)
-                val left = random.nextInt(columns)
-                val bottom = top + size - 1
-                val right = left
+                val top: Int
+                val left: Int
+                val bottom: Int
+                val right: Int
+                if (direction) {
+                    // Horizontal placement
+                    top = random.nextInt(rows)
+                    left = random.nextInt(columns - size + 1)
+                    bottom = top
+                    right = left + size - 1
+                } else {
+                    // Vertical placement
+                    top = random.nextInt(rows - size + 1)
+                    left = random.nextInt(columns)
+                    bottom = top + size - 1
+                    right = left
+                }
                 ship = StudentShip(top, left, bottom, right)
             } while (ships.any { it.overlaps(ship) } || bottom > rows - 1)
             ships.add(ship)
         }
         return StudentBattleshipOpponent(columns, rows, ships)
     }
-
     private fun Ship.coversCoordinate(coord: Coordinate): Boolean {
         return coord.x in left..right && coord.y in top..bottom
     }
