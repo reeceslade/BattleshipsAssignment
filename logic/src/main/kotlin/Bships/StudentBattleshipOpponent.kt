db83518e33d45bb354b37d6bfc41278cabe5b85d
reeceslade <reecesladey@outlook.com>
2023-04-15 10:21:10 +0100

    Revert "still failing 5 tests"
    This reverts commit 7b836e66

--- logic/src/main/kotlin/Bships/StudentBattleshipOpponent.kt
+++ logic/src/main/kotlin/Bships/StudentBattleshipOpponent.kt
@@ -1,80 +1,41 @@
 package Bships
-
 import uk.ac.bournemouth.ap.battleshiplib.BattleshipOpponent
 import uk.ac.bournemouth.ap.battleshiplib.Ship
 import uk.ac.bournemouth.ap.lib.matrix.ext.Coordinate
 import kotlin.random.Random
 
+
 class StudentBattleshipOpponent(
     override val columns: Int,
     override val rows: Int,
     override val ships: List<Ship>
-) : BattleshipOpponent {
-    // need to check every ship
-    private val shipSizes = intArrayOf(2, 3, 3, 4, 5)
-    private val random = Random
 
-    init {
-        checkShips(ships, columns, rows)
+) : BattleshipOpponent {
+    //need to check every ship
+    private fun checkShipsOverlap(ships: List<Ship>): Boolean {
+        for (i in ships.indices) {
+            for (j in i + 1 until ships.size) {
+                val ship1 = ships[i]
+                val ship2 = ships[j]
+                val leftOverlap = (ship1.left..ship1.right).intersect(ship2.left..ship2.right).count()
+                val topOverlap = (ship1.top..ship1.bottom).intersect(ship2.top..ship2.bottom).count()
+                if (leftOverlap > 0 && topOverlap > 0) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
-    companion object {
-        fun randomOpponent(
-            columns: Int,
-            rows: Int,
-            shipSizes: IntArray,
-            random: Random
-        ): StudentBattleshipOpponent {
-            val ships = mutableListOf<Ship>()
-            for (size in shipSizes) {
-                var ship: Ship
-                var isValidPlacement: Boolean
-                do {
-                    val isVertical = random.nextBoolean()
-                    val left = random.nextInt(columns)
-                    val top = random.nextInt(rows)
-                    val bottom = if (isVertical) top + size - 1 else top
-                    val right = if (isVertical) left else left + size - 1
-                    ship = StudentShip(top, left, bottom, right)
-                    isValidPlacement = !ships.any { it.overlaps(ship) } &&
-                            left < columns && right < columns && top < rows && bottom < rows
-                } while (!isValidPlacement)
-                ships.add(ship)
-            }
-            return StudentBattleshipOpponent(columns, rows, ships)
-        }
 
-        private fun checkShips(ships: List<Ship>, columns: Int, rows: Int) {
-            ships.forEach { ship ->
-                require(ship.top in 0 until rows) {
-                    throw IllegalArgumentException("Ship $ship is not in bounds")
-                }
-                require(ship.bottom in 0 until rows) {
-                    throw IllegalArgumentException("Ship $ship is not in bounds")
-                }
-                require(ship.left in 0 until columns) {
-                    throw IllegalArgumentException("Ship $ship is not in bounds")
-                }
-                require(ship.right in 0 until columns) {
-                    throw IllegalArgumentException("Ship $ship is not in bounds")
-                }
-                ships.filter { it !== ship }.forEach { otherShip ->
-                    require(!ship.overlaps(otherShip)) {
-                        throw IllegalArgumentException("Ships $ship and $otherShip overlap")
-                    }
-                }
-            }
-        }
+    init {
+        require(ships.all { it.top in 0 until rows && it.bottom in 0 until rows && it.left in 0 until columns && it.right in 0 until columns }) { "Ships must be within the bounds of the game board" }
+        require(!checkShipsOverlap(ships)) { "Ships cannot overlap" }
+    }
 
-        private fun Ship.overlaps(other: Ship): Boolean {
-            val xOverlap = this.left <= other.right && this.right >= other.left
-            val yOverlap = this.top <= other.bottom && this.bottom >= other.top
-            return xOverlap && yOverlap
-        }
 
-        private fun Ship.coversCoordinate(coord: Coordinate): Boolean {
-            return coord.x in left..right && coord.y in top..bottom
-        }
+    private fun Ship.coversCoordinate(coord: Coordinate): Boolean {
+        return coord.x in left..right && coord.y in top..bottom
     }
 
     override fun shipAt(column: Int, row: Int): BattleshipOpponent.ShipInfo<Ship>? {
--- logic/src/main/kotlin/Bships/StudentShip.kt
+++ logic/src/main/kotlin/Bships/StudentShip.kt
@@ -9,29 +9,17 @@ import uk.ac.bournemouth.ap.lib.matrix.ext.Coordinate
     override val bottom: Int,
     override val right: Int
 ) : Ship {
-
      fun overlaps(other: StudentShip): Boolean {
-         require(top in 0..9) { "Invalid top coordinate" }
-         require(left in 0..9) { "Invalid left coordinate" }
-         require(bottom in 0..9) { "Invalid bottom coordinate" }
-         require(right in 0..9) { "Invalid right coordinate" }
-
-         if (this.top > other.bottom || this.bottom < other.top || this.left > other.right || this.right < other.left) {
-             // The ships do not overlap
-             return false
-         }
-
          val leftOverlap = (left..right).intersect(other.left..other.right).count()
          val topOverlap = (top..bottom).intersect(other.top..other.bottom).count()
          return leftOverlap > 0 && topOverlap > 0
      }
  }
 
+val carrier = StudentShip(6, 6, 0, 4) // creates a ship at coordinates (0,0) to (0,4)
+val battleship = StudentShip(1, 1, 4, 1) // creates a ship at coordinates (1,1) to (4,1)
+val cruiser = StudentShip(2, 2, 4, 2) // creates a ship at coordinates (2,2) to (4,2)
+val submarine = StudentShip(3, 3, 5, 3) // creates a ship at coordinates (3,3) to (5,3)
+val destroyer = StudentShip(4, 4, 5, 4) // creates a ship at coordinates (4,4) to (5,4)
 
-     val carrier = StudentShip(6, 6, 0, 4) // creates a ship at coordinates (0,0) to (0,4)
-     val battleship = StudentShip(1, 1, 4, 1) // creates a ship at coordinates (1,1) to (4,1)
-     val cruiser = StudentShip(2, 2, 4, 2) // creates a ship at coordinates (2,2) to (4,2)
-     val submarine = StudentShip(3, 3, 5, 3) // creates a ship at coordinates (3,3) to (5,3)
-     val destroyer = StudentShip(4, 4, 5, 4) // creates a ship at coordinates (4,4) to (5,4)
-
-     val ships = listOf(carrier, battleship, cruiser, submarine, destroyer)
+val ships = listOf(carrier, battleship, cruiser, submarine, destroyer)
