12ebe301f12970f0dafcd99583668ab51413e6b1
reeceslade <reecesladey@outlook.com>
2023-04-18 16:26:04 +0100

    next stages of implementation: add in opponent guessing, player dragging ships, iterative take turns, ga,e over, additional buttons and computer difficulty

--- app/src/main/java/uk/ac/bournemouth/ap/battleships/MainActivity.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/battleships/MainActivity.kt
@@ -2,10 +2,21 @@ package uk.ac.bournemouth.ap.battleships
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+
+        val playGameButton = findViewById<Button>(R.id.playGameBtn)
+        playGameButton.setOnClickListener {
+            val homeView = HomeView(this)
+            setContentView(homeView)
+        }
+        val exitGameButton = findViewById<Button>(R.id.btn_exit)
+        exitGameButton.setOnClickListener {
+            finish() // Finish the activity, which will exit the game
+        }
     }
 }
