d10c593a6fc13fd36fb127bfef16a4ad8ff208ec
reeceslade <reecesladey@outlook.com>
2023-04-20 15:59:19 +0100

    fiddling with touchEvent

--- app/src/main/java/uk/ac/bournemouth/ap/battleships/MainActivity.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/battleships/MainActivity.kt
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         val playGameButton = findViewById<Button>(R.id.playGameBtn)
         playGameButton.setOnClickListener {
-            val homeView = HomeView(this)
-            setContentView(homeView)
+            val homeView2 = HomeView2(this)
+            setContentView(homeView2)
         }
         val exitGameButton = findViewById<Button>(R.id.btn_exit)
         exitGameButton.setOnClickListener {
