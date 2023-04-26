1c49daad15548133165c2f1ddf90cd10cd170e83
reeceslade <reecesladey@outlook.com>
2023-04-26 16:39:37 +0100

    viewbinding starting to work

--- app/src/main/java/uk/ac/bournemouth/ap/battleships/MainActivity.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/battleships/MainActivity.kt
@@ -3,22 +3,35 @@ package uk.ac.bournemouth.ap.battleships
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import uk.ac.bournemouth.ap.battleships.databinding.ActivityHomeBinding
+import uk.ac.bournemouth.ap.battleships.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         supportActionBar?.hide()
-        val playGameButton = findViewById<Button>(R.id.playGameBtn)
-        playGameButton.setOnClickListener {
-          //  val shipPositions = listOf<Pair<Int, Int>>(/* Add your ship positions here */)
-            val homeView2 = HomeView2(this)
-            setContentView(homeView2)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.playGameBtn.setOnClickListener { onPlayBtnClick() }
         }
+    private fun onPlayBtnClick() {
+        val testFragment = FragmentTest()
 
-        val exitGameButton = findViewById<Button>(R.id.btn_exit)
+        // Begin a fragment transaction
+        supportFragmentManager.beginTransaction()
+            // Replace the fragment container with the TestFragment
+            .replace(R.id.fragmentContainer, testFragment)
+            // Add the transaction to the back stack (optional)
+            .addToBackStack(null)
+            // Commit the transaction
+            .commit()
+    }
+
+     /*   val exitGameButton = findViewById<Button>(R.id.btn_exit)
         exitGameButton.setOnClickListener {
             finish() // Finish the activity, which will exit the game
-        }
+        }*/
     }
-}
